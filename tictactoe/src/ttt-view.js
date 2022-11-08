27d018794ec17ecb7e861415aee2a1b839a5c186
Arnob <arnobdam@gmail.com>
2022-11-08 16:10:07 -0500

    working on setupBoard

--- tictactoe/src/ttt-view.js
+++ tictactoe/src/ttt-view.js
@@ -1,13 +1,27 @@
+// const Board = require("/ttt_node/board.js");
+
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.el = el;
+    this.setupBoard();
+  }
 
-  setupBoard() {}
-  
-  bindEvents() {}
+  setupBoard() {
+    const grid = document.createElement("ul");
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let li = document.createElement("li");
+        grid.appendChild(li);
+      }
+    }
+    this.el.appendChild(grid);
+  }
 
-  handleClick(e) {}
+  bindEvents() { }
 
-  makeMove(square) {}
+  handleClick(e) { }
+
+  makeMove(square) { }
 
 }
 
