aa29ddc3daf24ebf4af1f7fb200b175f29b00e83
kkj2010 <kunjukim0210@gmail.com>
2022-11-08 14:05:16 -0500

    working on index.js

---
+++ tictactoe/src/index.js
@@ -0,0 +1,10 @@
+const View = require('./ttt-view.js')// require appropriate file
+const Game = require('/ttt_node/game.js') // require appropriate file
+
+document.addEventListener("DOMContentLoaded", () => {
+    let game= new Game()
+    let view= new View(game, )
+  
+  // Your code here
+});
+
