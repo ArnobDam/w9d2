1c97b175cf9cd23c5f24ef181785ead262070a09
kkj2010 <kunjukim0210@gmail.com>
2022-11-08 14:24:59 -0500

    still working on index.js

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -3,7 +3,8 @@ const Game = require('/ttt_node/game.js') // require appropriate file
 
 document.addEventListener("DOMContentLoaded", () => {
     let game= new Game()
-    let view= new View(game, )
+    let figure= document.querySelector(".ttt")
+    let view= new View(game, figure)
   
   // Your code here
 });
